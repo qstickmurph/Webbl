5b494cdf0262e5cd18aa4de527f6426f4a1ab6a4
qstickmurph <quinn@stickmurph.com>
2025-02-12 18:45:41 -0700

    Added RouterOutlet and basic componenets to frontend

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavBarComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'web';
+export class AppRootComponent {
+
 }
+
--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'game', component: GameComponent},
+  {path: 'profile', component: ProfileComponent}
+];
