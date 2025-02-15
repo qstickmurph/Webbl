4fa28f463150b4a1d13ea547e6e5bffe97ef0150
qstickmurph <quinn@stickmurph.com>
2025-02-15 09:53:37 -0700

    Removed NavBarButtonComponent and fixed NavBar styling

--- web/src/app/nav-bar/nav-bar-buttons/nav-bar-buttons.component.ts
+++ web/src/app/nav-bar/nav-bar-buttons/nav-bar-buttons.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { defaultNavBarButtonsConstant } from '../../constants/nav-bar-constants';
 import { NavBarButton } from '../../models/nav-bar-button.model';
-import { NavBarButtonComponent } from './nav-bar-button/nav-bar-button.component';
 
 @Component({
   selector: 'nav-bar-buttons',
   imports: [
-    NavBarButtonComponent,
-    NgFor
+    NgFor,
+    RouterLink
   ],
   templateUrl: './nav-bar-buttons.component.html',
   styleUrl: './nav-bar-buttons.component.scss'
