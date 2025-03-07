0302d5f571c0f39644f8533fda73a2229c3e58ec
qstickmurph <quinn@stickmurph.com>
2025-03-06 17:43:25 -0700

    Added page not found component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { PlayPageComponent } from './play-page/play-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   {path: 'home', component: HomePageComponent},
-  {path: 'play', component: PlayPageComponent}
+  {path: 'play', component: PlayPageComponent},
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**',   redirectTo: '404', pathMatch: 'full' }
 ];
