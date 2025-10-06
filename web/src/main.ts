d45b40a538896e1f60bad75eeda1750566c80dc5
qstickmurph <quinn@stickmurph.com>
2025-10-05 23:07:23 -0600

    Updated npm packages

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppRootComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppRootComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppRootComponent, appConfig);
